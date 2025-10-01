ce6b70e52a8fc0352a8e604788563988c53dd243
xaviercooper <xcooper@my.yearupunited.org>
2025-10-01 15:55:59 -0400

    sting of names

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,15 +1,25 @@
 package com.pluralsight;
-public class Main {
+
+import java.util.Scanner;
+
+    public class Main {
+
+        public static void main(String[] args) {
+
+Scanner scanner = new Scanner (System.in);
+       System.out.println("first name goes here ") ;
+
+String firstname = scanner.nextLine();
+
+System.out.println("middle name goes here ");
+            String middlename = scanner.nextLine();
+
+            System.out.println("last name goes here");
+            String lastname = scanner.nextLine();
 
 
 
-public static void main(String[] args) {
+String fullname = firstname+" "+middlename+" "+lastname;;
 
-
-    System.out.println("program goes here...");
-
-
-}
-
-}
-
+System.out.println(fullname);
+        }}
